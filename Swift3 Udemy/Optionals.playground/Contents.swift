dfadeafbbbbd27f81139845595308196708afdca
Colin <colin.cook@cox.net>
2017-01-13 18:06:37 -0600

    adding optionals file

---
+++ Swift3 Udemy/Optionals.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myStr: String?
+
+print(myStr)
+
+myStr = "Colin"
+//If you can get a value out of myStr do the following
+if let myStrVal = myStr {
+    var newStr = myStrVal + " Cook"
+}
+
+
