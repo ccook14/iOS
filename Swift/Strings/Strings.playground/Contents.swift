ccd9aabd35fd8c23f6d5fcbc505bd838f3ba0df8
Colin <colin.cook@cox.net>
2017-01-14 12:05:27 -0600

    adding strings file

---
+++ Swift/Strings/Strings.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str: String = "Hello, playground"
+
+var firstName = "Colin"
+var lastName = "Cook"
+
+
+var fullName = firstName + " " + lastName
+var fullName2 = "\(firstName) \(lastName)"
--- Swift/Variables/Variables.playground/Contents.swift
+++ Swift/Variables/Variables.playground/Contents.swift
@@ -2,4 +2,26 @@
 
 import UIKit
 
-var str = "Hello, playground"
+
+//Binary
+var message = "Hello, playground"
+
+//Operators 
+
+//Unary, Binary , Ternary
+// one object, two objects, three objects
+
+var amICool = true
+
+//Unary
+amICool = !amICool
+
+var feelGoodAboutMyself = true
+
+feelGoodAboutMyself = amICool ? true : false
+//If amICool is true feelGoodAboutMyself is true
+
+var bankAccountBalance = 1000
+var cashRegisterMessage = bankAccountBalance >= 50 ? "You just bought the item" : "You are broke as a joke"
+//Another example of ternary operator
+
