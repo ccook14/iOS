cd9cf65a5a10052506c387484e9dd3833a979d56
Colin <colin.cook@cox.net>
2017-01-13 19:45:09 -0600

    adding classes and objects file

--- Swift3 Udemy/ClassesAndObjects.playground/Contents.swift
+++ Swift3 Udemy/ClassesAndObjects.playground/Contents.swift
@@ -2,4 +2,22 @@
 
 import UIKit
 
-var str = "Hello, playground"
+  class FastCar {
+    
+    var topSpeed = Int()
+    
+    init (topSpeed: Int) {
+        self.topSpeed = topSpeed
+    }
+    
+    func GetTopSpeed () -> Int {
+        return self.topSpeed
+    
+    }
+  }
+
+  var myCar = FastCar(topSpeed: 200)
+  myCar.GetTopSpeed()
+  
+  var Lexus = FastCar(topSpeed: 220)
+  Lexus.GetTopSpeed()
