a32ead8f5e1c0adce85c08514fcb4ced64e7761b
Colin <colin.cook@cox.net>
2017-01-14 14:45:34 -0600

    adding strings file

--- Swift/Strings/Strings.playground/Contents.swift
+++ Swift/Strings/Strings.playground/Contents.swift
@@ -2,11 +2,32 @@
 
 import UIKit
 
-var str: String = "Hello, playground"
+var str = "Hello, playground"
 
 var firstName = "Colin"
 var lastName = "Cook"
 
-
+var age = 24
 var fullName = firstName + " " + lastName
-var fullName2 = "\(firstName) \(lastName)"
+var fullName2 = "\(firstName) \(lastName) is \(age)"
+
+//String interplemation 
+fullName.append(" III")
+
+var bookTitle = "revenge of the falcons"
+
+bookTitle = bookTitle.capitalized
+
+var annoyingCaps = "HAHAHHAHA PLEASE HELP ME NOW! HERE IS MY ISSUE"
+var lowerCasedChat = annoyingCaps.lowercased()
+
+//Fuck Ass, if we want to censor user input
+
+var sentence = "Fuck ass, Come on this is crazy!"
+
+if  sentence.contains("ass") || sentence.contains("Fuck"){
+    sentence.replacingOccurrences(of: "Fuck", with: "Fudge")
+    
+    sentence.replacingOccurrences(of: "ass", with: "butt")
+    
+}
