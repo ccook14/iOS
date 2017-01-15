65a034a524a18283eada23b818e9dca6e65e6f09
Colin <colin.cook@cox.net>
2017-01-14 22:25:47 -0600

    first excercise flex

---
+++ Swift/Excercises/Exercise1.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+//Create a variable with a name of your choosing that has a value of 15
+//Create a 2nd variable with a name of your choosing that has a value of 5
+//Create a 3rd variable that stores that sum of the 1st and 2nd variables (20 should print to the screen)
+//Create a 4th variable that stores the result of a multiplication operation - the stored result should be equal to 45
+//Create a 5th variable that stores the result of the 1st variable minus the 2nd variable - the result should be 10
+//Create a 6th variable that stores the result of the 1st variable divided by the 2nd variable - the result should be 3
+//Create 3 new variables of any name and of any value, but all on the same line
+//Create 3 constants (let) of any name and of any value, but all on the same line
+//Create an explicit variable of any name of type Double and don't give it any value
+//Create another explicit variable of any name of type Int but DO give it an initial value of 423
+//Create a variable of type String with a value of "Hello"
+//Create another variable of type String with a value of "World"
+//Create a 3rd variable to store the result of concatenating the first two strings (using the + symbol)
+//Using that 3rd variable again, store the result of concatenating the first two string but instead using "(someVar)" syntax (string interpolation)
+
+
+
+var NE = 15
+var HOU = 5
+
+var sum = NE + HOU
+
+var mult = NE * 3
+
+var diff = NE - HOU
+
+var div = NE / HOU
+
+var col1 = 14; var col2 = 8; var col3 = 10
+
+let atl1 = 1; let atl2 = 2; let atl3 = 404
+
+var atlWin: Double
+
+var str = "Hello"
+var str1 = "World"
+var str2 = str + " " + str1
+
+str2 = "\(str) \(str1)"
