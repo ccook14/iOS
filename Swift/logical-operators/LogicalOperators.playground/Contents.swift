5f1e8a2fa7ccd1c8df660bdc93b6582f2a89b97e
Colin <colin.cook@cox.net>
2017-01-18 18:20:20 -0600

    Logical Operators touch id

---
+++ Swift/logical-operators/LogicalOperators.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//This sort of logic could be used for Touch ID feature on iPhone
+
+
+//Logical NOT operator - unary prefix operator
+
+let allowedEntry = false
+
+//! in Front not true
+if !allowedEntry {
+    print("ACCESS DENIED")
+}
+
+let enterDoorCode = true
+let passedFingerPrintScan = false
+let iAmTomCruise = false
+
+
+//Checks iAmTomCruise first, then if not true checks other parameter
+if  (enterDoorCode  && passedFingerPrintScan) || iAmTomCruise {
+    print("Welcome")
+} else {
+    print("ACCES DENIED AGAIN")
+}
+
+let hasDoorKey = false
+let knowOverridePassowrd = true
+if hasDoorKey || knowOverridePassowrd {
+    print("Welcome!!!")
+} else {
+    print("YOU STILL AINT GETTING IN FOOL")
+}
